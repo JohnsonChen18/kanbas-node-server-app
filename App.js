0a5c745ae27967d40c139bceb7025c3f196bc1fe
MiaozhiChen <chen.miaoz@northeastern.edu>
2024-07-21 16:05:48 -0400

    first commit a6

--- App.js
+++ App.js
@@ -2,16 +2,21 @@ import express from 'express';
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
+import "dotenv/config";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 const app = express()
-
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
-Hello(app)
+Hello(app);
 Lab5(app);
 app.listen(process.env.PORT || 4000)
