fea750b26ca80277be70658a63426df48fd65b92
MiaozhiChen <chen.miaoz@northeastern.edu>
2024-07-22 19:55:28 -0400

    finish course, assigment and module

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
 import "dotenv/config";
-import CourseRoutes from "./Kanbas/Courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
+import CourseRoutes from "./Courses/routes.js";
+import ModuleRoutes from "./Modules/routes.js";
+import AssignmentRoutes from "./Assignments/routes.js";
 import session from "express-session";
 const app = express()
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
